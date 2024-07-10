da337fb75c01ef171c6ec705d42bb69c369acfc6
Samuel Berthe <dev@samuel-berthe.fr>
2024-07-10 15:37:40 +0200

    feat: when the name is empty, WithGroup returns the receiver

--- handler.go
+++ handler.go
@@ -93,6 +93,11 @@ func (h *WebhookHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
 }
 
 func (h *WebhookHandler) WithGroup(name string) slog.Handler {
+	// https://cs.opensource.google/go/x/exp/+/46b07846:slog/handler.go;l=247
+	if name == "" {
+		return h
+	}
+
 	return &WebhookHandler{
 		option: h.option,
 		attrs:  h.attrs,
